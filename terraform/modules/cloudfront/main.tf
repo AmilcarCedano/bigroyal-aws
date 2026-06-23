431189788760ef9e9d611ab2d7fdd0a4ceb33ffb
AmilcarCedano <Amilcar.cb.2015@gmail.com>
2026-06-22 23:52:14 -0500

    fix(cloudfront): restringe distribución a países de operación de BigRoyal

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -119,7 +119,8 @@ resource "aws_cloudfront_distribution" "this" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type  = "whitelist"
+      locations         = ["PE", "US", "CO", "MX", "CL", "AR"]
     }
   }
 
